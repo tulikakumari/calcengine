c8e45378644808d328b262a77503927c1d2a2a93
Tulika Kumari <tulikakashyap1999@gmail.com>
2020-04-21 18:55:00 +0530

    updated

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -7,7 +7,36 @@ public class MathEquation {
     public double result;
 
 
-     public void execute(){
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal)
+    {
+        this.leftVal = leftVal;
+
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+    public void setRightVal(double rightVal){
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(char opCode){
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void execute(){
          switch (opCode) {
              case 'a':
                  result = leftVal + rightVal;
